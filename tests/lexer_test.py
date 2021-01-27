d403d1f621e47946a0b462d2d2bea7b7d3dbec4c
Edinson_Salamanca <sennin7k@gmail.com>
2021-01-27 11:21:37 -0500

    building the lexer II

--- tests/lexer_test.py
+++ tests/lexer_test.py
@@ -68,7 +68,27 @@ class LexerTest(TestCase):
             Token(TokenType.LBRACE, '{'),
             Token(TokenType.RBRACE, '}'),
             Token(TokenType.COMMA, ','),
-            Token(TokenType.SEMICOLON,';')
+            Token(TokenType.SEMICOLON, ';')
         ]
 
         self.assertEquals(tokens, expected_tokens)
+
+    def test_assignment(self) -> None:
+        source: str = 'variable cinco = 5;'
+        lexer: Lexer = Lexer(source)
+
+        tokens: List[Token] = []
+        for i in range(5):
+            tokens.append(lexer.next_token())
+
+        expected_tokens: List[Token] = [
+            Token(TokenType.LET, 'variable'),
+            Token(TokenType.IDENT, 'cinco'),
+            Token(TokenType.ASSIGN, '='),
+            Token(TokenType.INT, '5'),
+            Token(TokenType.SEMICOLON, ';'),
+        ]
+
+        self.assertEquals(tokens, expected_tokens)
+    
+    
