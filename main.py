8da62fcf8c628a030fe16fec74d7d13f12000cac
Edinson_Salamanca <sennin7k@gmail.com>
2021-01-27 20:34:01 -0500

    first-repl

---
+++ main.py
@@ -0,0 +1,12 @@
+from lpp.repl import start_repl
+
+
+def main() -> None:
+    print('Bienvenido al lenguake de promación Plazi.')
+    print('Escribe una oración para comenzar.')
+
+    start_repl()
+
+
+if __name__ == '__main__':
+    main()
