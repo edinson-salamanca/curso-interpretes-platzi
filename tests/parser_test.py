e8f2555d63ef7c4177928a489a052f730479ebe1
Edinson_Salamanca <sennin7k@gmail.com>
2021-01-28 14:57:12 -0500

    parsing-let-statements

--- tests/parser_test.py
+++ tests/parser_test.py
@@ -1,7 +1,12 @@
+from typing import List, cast
 from unittest import TestCase
 
 from lpp.lexer import Lexer
-from lpp.ast import Program
+from lpp.ast import (
+    LetStatement,
+    Program, Statement,
+
+)
 from lpp.parser import Parser
 
 
@@ -16,3 +21,41 @@ class ParserTest(TestCase):
 
         self.assertIsNotNone(program)
         self.assertIsInstance(program, Program)
+
+    def test_let_statements(self):
+        source: str = '''
+            variable x = 5;
+            variable y = 10;
+            variable foo = 20;
+        '''
+        lexer: Lexer = Lexer(source)
+        parser: Parser = Parser(lexer)
+
+        program: Program = parser.parse_program()
+
+        self.assertEqual(len(program.statements), 3)
+
+        for statement in program.statements:
+            self.assertEqual(statement.token_literal(), 'variable')
+            self.assertIsInstance(statement, LetStatement)
+
+    def test_names_in_let_statements(self) -> None:
+        source: str = '''
+            variable x = 5;
+            variable y = 10;
+            variable foo = 20;
+        '''
+        lexer: Lexer = Lexer(source)
+        parser: Parser = Parser(lexer)
+
+        program: Program = parser.parse_program()
+
+        names: List[str] = []
+
+        for statement in program.statements:
+            statement = cast(LetStatement, statement)
+            assert statement.name is not None
+            names.append(statement.name.value)
+
+        expected_names: List[str] = ['x', 'y', 'foo']
+        self.assertEquals(names, expected_names)
